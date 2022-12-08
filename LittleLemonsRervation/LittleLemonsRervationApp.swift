fa1ba3c08654fef4ad1723c923c88c14c7104962
Fedi Nabli <fedinabli@gmail.com>
2022-12-08 19:59:37 +0100

    Initial Commit

---
+++ LittleLemonsRervation/LittleLemonsRervationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonsRervationApp.swift
+//  LittleLemonsRervation
+//
+//  Created by Fedi Nabli on 8/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonsRervationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
