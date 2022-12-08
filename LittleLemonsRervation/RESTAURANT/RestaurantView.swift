efdd8a458a10d8a877c8702c6df4cd9b394623f7
Fedi Nabli <fedinabli@gmail.com>
2022-12-08 21:54:56 +0100

    Added Restaurant View

---
+++ LittleLemonsRervation/RESTAURANT/RestaurantView.swift
@@ -0,0 +1,38 @@
+//
+//  RestaurantView.swift
+//  LittleLemonsRervation
+//
+//  Created by Fedi Nabli on 8/12/2022.
+//
+
+import SwiftUI
+
+struct RestaurantView: View {
+    private var restaurant: RestaurantLocation
+    
+    init(_ restaurant: RestaurantLocation) {
+        self.restaurant = restaurant
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 3) {
+            Text(restaurant.city)
+                .font(.title2)
+            
+            HStack {
+                Text(restaurant.neighborhood)
+                Text("-")
+                Text(restaurant.phoneNumber)
+            }
+            .font(.caption)
+            .foregroundColor(.gray)
+        }
+    }
+}
+
+struct RestaurantView_Previews: PreviewProvider {
+    static var previews: some View {
+        let sampleRestaurant = RestaurantLocation(city: "Las Vegas", neighborhood: "Downtown", phoneNumber: "(702) 555-9898")
+        RestaurantView(sampleRestaurant)
+    }
+}
